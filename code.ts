b05266a1078691842f08576295a6e7a618d0b41e
Joseph Wright <jtewright@gmail.com>
2020-05-13 14:41:19 -0700

    not just frames, moves viewport to show selected frame

--- code.ts
+++ code.ts
@@ -2,37 +2,43 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = msg => {
-
-    // Next
-    if (msg.type === 'next') {
-        const page = figma.currentPage;
-        const selected = page.selection;
-        const frames = page.findAll(node => node.type === "FRAME");
-        let skip = false;
-        if (frames.length == 0) {
-            figma.notify("No Frames to be found");
-            skip = true;
-        };
-        //nothing selected
-        if (selected.length == 0 && !skip) {
-            page.selection = [frames[0]];
-        }
-        //one thing selected
-        else if (selected.length == 1 && !skip) {
-            const selectedNode = selected[0];
-            const selectedIndex = frames.indexOf(selectedNode);
-            let nextIndex = selectedIndex + 1;
-            if (nextIndex >= frames.length) {
-                nextIndex = 0;
-            }
-            page.selection = [frames[nextIndex]];
-        }
-        else if (!skip) {
-            page.selection = [frames[0]];
-        }
-    }
-
-    if (msg.type === 'exit') {
-        figma.closePlugin();
+    switch (msg.type) {
+        case 'next': 
+            nextNode();
+            break;
+        case 'exit':
+            figma.closePlugin();
+            break;
+        default:
     }
 };
+
+function nextNode() {
+    const page = figma.currentPage;
+    const selected = page.selection;
+    const nodes = page.findAll();
+    let newSelection = [];
+
+    // no frames
+    if (nodes.length == 0) {
+        figma.notify("Nothing to be found");
+        return;
+    }
+    // one selected
+    if (selected.length == 1) {
+        const selectedNode = selected[0];
+        const selectedIndex = nodes.indexOf(selectedNode);
+        let nextIndex = selectedIndex + 1;
+        if (nextIndex >= nodes.length) {
+            nextIndex = 0;
+        }
+        newSelection = [nodes[nextIndex]];
+    }
+    // many|none selected
+    else {
+        newSelection = [nodes[0]];
+    }
+
+    page.selection = newSelection;
+    figma.viewport.scrollAndZoomIntoView(newSelection);
+}
